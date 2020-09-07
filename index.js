b43a270aae8975bfa16f8e90152b424e6e707ce8
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-09-06 21:17:44 -0700

    added simple bot

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.BOT_USER_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+
+// Listens to incoming messages that contain "hello"
+app.message('Hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('set timer', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Timer set`);
+
+  setTimeout(async () => await say(`You time is over`),  5000)
+});
+
+// All the room in the world for your code
+
+
+(async () => {
+  // Start your app
+  await app.start(3000);
+
+  console.log('⚡️ Bolt app is running!');
+
+})();
