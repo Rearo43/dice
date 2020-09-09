7b559dcfc72996d75253826d500f435680ce7f5c
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-09-09 03:35:42 -0700

    tests for helper function currently passing

--- __tests__/src/modules/dieRoll.test.js
+++ __tests__/src/modules/dieRoll.test.js
@@ -1,22 +1,24 @@
 'use strict';
 
 const randomSpy = jest.spyOn(Math, 'random');
-const dieRoll = require('../../../src/modules//dieRoll.js');
+const { dieRoll } = require('../../../src/modules//dieRoll.js');
+const { eachRoll } = require('../../../src/modules//dieRoll.js');
 
 afterAll(() => {
   randomSpy.mockRestore();
 });
 
-it('Should roll the number 5', () => {
-  randomSpy.mockImplementation(() => 5);
-
-});
-it('Should run defaults', () => {
-  expect(dieRoll()).toBeGreaterThanOrEqual(1);
-  expect(dieRoll()).toBeLessThan(7);
+it('Should run helper function with default values', () => {
+  expect(eachRoll()).toBeGreaterThanOrEqual(1);
+  expect(eachRoll()).toBeLessThan(7);
 });
 
-it('Should run defaults', () => {
-  expect(dieRoll(3)).toBeGreaterThanOrEqual(1);
-  expect(dieRoll(3)).toBeLessThan(4);
+it('Should roll a 3', () => {
+  randomSpy.mockImplementation(() => 3);
+
 });
+
+// it('Should run defaults', () => {
+//   expect(dieRoll(3)).toBeGreaterThanOrEqual(1);
+//   expect(dieRoll(3)).toBeLessThan(4);
+// });
--- src/modules/dieRoll.js
+++ src/modules/dieRoll.js
@@ -1,52 +1,34 @@
 'use strict';
 
 
-function dieRoll(command) {
-  // '/dieroll 12 1'
+function dieRoll() {
   let input = command.text;
   let mid = input.indexOf(' ');
-  // let sides = parseInt(input.slice(0, mid));
-  // let rolls = parseInt(input.slice(mid, (input.length)));
-  console.log(sides, rolls);
-  // command.text
-  // let sides = 6;
-  // let rolls = 1;
-  let average = 0;
-  function eachRoll(rolls = 6, sides = 1) {
-    for(let i = 0; i < rolls; i++) {
-      let num = Math.floor(Math.random() * sides + 1);
-  
-      average += num;
-    }
-  }
 
   if(!command.text){
-    eachRoll();
+    return`The number ${eachRoll()} was rolled.`;
+
   } else {
     let sides = parseInt(input.slice(0, mid));
     let rolls = parseInt(input.slice(mid, (input.length)));
-  }
+    let answerBack = Math.floor(eachRoll(sides, rolls) / rolls);
 
-  // for(let i = 0; i < rolls; i++) {
-  //   let num = Math.floor(Math.random() * sides + 1);
-
-  //   average += num;
-  // }
-
-  if(rolls === 1) {
-    return`You rolled a/an ${average}`;
-  } else {
-    let answerBack = Math.floor(average / rolls);
-    return `The average sum of your rolls is ${answerBack}`;
+    return `The average sum of your die rolls is ${answerBack}`;
   }
 }
 
-module.exports = dieRoll;
 
-// function eachRoll(rolls = 6, sides = 1) {
-//   for(let i = 0; i < rolls; i++) {
-//     let num = Math.floor(Math.random() * sides + 1);
 
-//     average += num;
-//   }
-// }
+function eachRoll(sides = 6, rolls = 1) {
+  let average = 0;
+
+  for(let i = 0; i < rolls; i++) {
+    let num = Math.floor(Math.random() * sides + 1);
+
+    average += num;
+  }
+   
+  return average;
+}
+
+module.exports = {dieRoll, eachRoll};
