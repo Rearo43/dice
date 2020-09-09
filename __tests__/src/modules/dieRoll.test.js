763778568ba8356d98fd089cbe4e13de33b59fba
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-09-09 14:49:03 -0700

    dieRoll tests cleaned up and cover 100percent of file

--- __tests__/src/modules/dieRoll.test.js
+++ __tests__/src/modules/dieRoll.test.js
@@ -1,38 +1,27 @@
 'use strict';
 
-const randomSpy = jest.spyOn(Math, 'random');
 const _ = require('lodash');
 jest.mock('lodash');
+
 const { dieRoll, eachRoll } = require('../../../src/modules//dieRoll.js');
 
-afterEach(() => {
-  randomSpy.mockRestore();
-});
-
-it.only('Should run helper function with default values', () => {
+it('Should run helper function with default values', () => {
   _.random.mockImplementation(() => 3);
+
   expect(eachRoll()).toBeGreaterThanOrEqual(1);
   expect(eachRoll()).toBeLessThan(7);
 });
 
-it('Should roll a 3', () => {
-  randomSpy.mockImplementation(() => 3);
-//   randomSpy.mockRestore();
-});
-
 it('Should run with user input', () => {
-//   randomSpy.mockImplementation(() => 2);
   _.random.mockImplementation(() => 2);
-  let actual = dieRoll('');
-  let expected = 'The number 2 was rolled.';
-  expect(actual).toEqual(expected);
-//   expect(dieRoll('')).toMatch('The number 4 was rolled.');
-//   randomSpy.mockRestore();
+
+  expect(dieRoll('')).toEqual('The number 2 was rolled.');
 });
 
 it('Should run with user input', () => {
-  let userInput = '5 4';
   _.random.mockImplementation(() => 3);
+
+  let userInput = '5 4';
+  
   expect(dieRoll(userInput)).toMatch('The average sum of your die rolls is 3.');
-//   randomSpy.mockRestore();
 });
