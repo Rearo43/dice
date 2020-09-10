3479744afbf701bd83b4c2430dca7365906b18e2
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-09-10 15:01:58 -0700

    has to refactor some tests because of syntax changes in file

--- __tests__/src/modules/dieRoll.test.js
+++ __tests__/src/modules/dieRoll.test.js
@@ -5,8 +5,8 @@ jest.mock('lodash');
 
 const { dieRoll, eachRoll } = require('../../../src/modules//dieRoll.js');
 
-const slackObjEmpty = {text: ''};
-const slackObjInput = {text: '5 4'};
+const slackObjEmpty = {text: '', user_id: '@Reagan'};
+const slackObjInput = {text: '5 4', user_id: '@Reagan'};
 
 describe('Testing Die Roll Module: ', () => {
 
@@ -20,13 +20,13 @@ describe('Testing Die Roll Module: ', () => {
     it('Should run with default values', () => {
       _.random.mockImplementation(() => 2);
 
-      expect(dieRoll(slackObjEmpty)).toEqual(':die-2:');
+      expect(dieRoll(slackObjEmpty)).toEqual('<@@Reagan> rolled :die-2:');
     });
 
     it('Should run with user input', () => {
       _.random.mockImplementation(() => 3);
 
-      expect(dieRoll(slackObjInput)).toMatch('After 4 rolls, the average of the dice rolled was... :die-3:');
+      expect(dieRoll(slackObjInput)).toMatch('<@@Reagan> rolled a die and after 4 rolls, the average of the dice rolled was... :die-3:');
     });
 
 });
