c6376921822e225d61c923720fe35150428312ec
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-09-11 19:44:11 -0700

    added ability to render multiple dice

--- src/modules/dieRoll.js
+++ src/modules/dieRoll.js
@@ -11,20 +11,24 @@ const _ = require('lodash');
 //user input '/dieroll two 3' <- want to change input to roll two die instead of choose sides
 
 function dieRoll(command) {
-    let userInput = command.text;
-    let mid = userInput.indexOf(' ');
+  let userInput = command.text;
+  let mid = userInput.indexOf(' ');
 
-    if (!userInput) {
-        return `<@${command.user_id}> rolled :die-${eachRoll()}:`;
-    }
+  if (!userInput) {
+    return `<@${command.user_id}> rolled ${eachRoll()}`;
+  }
 
-    else {
-        let sides = parseInt(userInput.slice(0, mid));
-        let rolls = parseInt(userInput.slice(mid, (userInput.length)));
-        let answerBack = eachRoll(sides, rolls);
+  else {
+    let sides = parseInt(userInput);
+    // let rolls = parseInt(userInput.slice(mid, (userInput.length)));
+    
 
-        return `<@${command.user_id}> rolled a die and after ${rolls} rolls, the average of the dice rolled was... :die-${answerBack}:`;
-    }
+    // let sides = parseInt(userInput.slice(0, mid));
+    // let rolls = parseInt(userInput.slice(mid, (userInput.length)));
+    // let answerBack = eachRoll(sides, rolls);
+
+    return `<@${command.user_id}> rolled a die and after rolls, the average of the dice rolled was... ${eachRoll(sides)}`;
+  }
 }
 
 
@@ -32,18 +36,19 @@ function dieRoll(command) {
  * eachRoll acts as a helper function to generate a random number for the die roll outcome.
  * @param {Number} sides either the default value of six or the users first input in their request.
  * @param {Number} rolls either the default value of one or the users second input in their request.
- * @returns {Number} returns the random number rolled or the average of all random numbers rolled when the parameter roll is greater than 1.
+ * @returns {String} returns the random number rolled or the average of all random numbers rolled when the parameter roll is greater than 1.
  */
-function eachRoll(sides = 6, rolls = 1) {
-    let total = 0;
+function eachRoll(rolls = 1) {
+//   let total = 0;
+  let str = '';
+  for (let i = 0; i < rolls; i++) {
+    let num = _.random(1, 6);
+    let newDie = `:die-${num}: `;
 
-    for (let i = 0; i < rolls; i++) {
-        let num = _.random(1, sides);
+    str+= newDie;
+  }
 
-        total += num;
-    }
-
-    return Math.floor(total / rolls);
+  return str;
 }
 
 
